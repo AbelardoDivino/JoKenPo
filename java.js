c7eb03a7ff7dd1f6a74f8578f163a0a6ad4cd73e
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-13 11:13:47 -0300

    add Add images of the respective game functions and create a click event in both for the JavaScript code.

---
+++ java.js
@@ -0,0 +1,12 @@
+let jogadas = ['1 pedra', '2 mao', '3 papel']
+
+let jogar = document.getElementById("jogar")
+let input = document.getElementById("put")
+let player2btn = document.getElementById("2player")
+let playpcbtn = document.getElementById("playvspc")
+
+jogar.addEventListener("click",()=>{
+
+    
+
+})
