b72974cafc9e6c1132c69a9265d73d0d59dacf7d
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-16 20:54:12 -0300

    update

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+
+
 // let jogadas = ['1 pedra', '2 tesoura', '3 papel']
 let jogadas = [1,2,3]
 
@@ -5,6 +7,10 @@ let jogar = document.getElementById("jogar")
 let input = document.getElementById("put")
 let player2btn = document.getElementById("2player")
 let playpcbtn = document.getElementById("playvspc")
+let jogarcomimagen = document.getElementById("jogarcomimagen")
+let fotopedra = document.getElementById("fotopedra")
+let fotopapal = document.getElementById("fotopapel")
+let fototesoura = document.getElementById("fototesoura")
 
 player2btn.addEventListener("click",()=>{
 
@@ -62,6 +68,26 @@ else if (jogador === 3 && pc === 1) {
 
 else{
     alert("pc venceu")
+}
+
+})
+
+jogarcomimagen.addEventListener("click",()=>{
+
+let modo = Number(prompt("digite o modo de jogo [1 - pvp 2 - vs pc]"))
+
+if (modo === 1) {
+    //imagen representando um numero para vereficar se venceu ou nao
+    // if () {
+        
+    // }
+
+
+
+}
+
+else{
+alert("sem amigos lkkkkkkkkkkkkkk")
 }
 
 })
